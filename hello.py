9d81d3a4cab59d06c727499dcb7a664056f6dcaa
chenhao <ronlimch87@gmail.com>
2022-07-29 04:00:01 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,11 @@
+def add(x, y):
+    return x + y
+
+
+result = add(1, 2)
+print(f"This sis the sum: 1, 2, {result}")
+
+# x = 1
+# y = 2
+# result = add(x,y)
+# print(f"This sis the sum: {x}, {y}, {result}")
